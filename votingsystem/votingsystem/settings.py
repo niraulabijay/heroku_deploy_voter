b18aab017a2e2a41fc1cd28f3434a8cf0eafdf53
Your Name <bijayfriend45@gmail.com>
2020-05-11 23:47:45 +0545

    vote not found

--- votingsystem/votingsystem/settings.py
+++ votingsystem/votingsystem/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vote'
+    'votingsystem.vote.apps.VoteConfig'
 ]
 
 MIDDLEWARE = [
